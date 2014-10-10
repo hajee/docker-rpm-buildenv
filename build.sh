2699e89c33c5efc9de9885039935226b76316a0e
Youssef Bagoulla <Youssef.Bagoulla@axiosengineering.com>
2014-10-10 09:06:11 -0400

    Added CentOS7 and general improvments to script

--- build.sh
+++ build.sh
@@ -2,18 +2,34 @@
 #for now, assume relative path to specfile is provided as argument
 #and the sources are the working directory of the container
 
+function test {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "error with $1" >&2
+        exit $status
+    fi
+    return $status
+}
+
 #source and display the build environment
 . ~/.bashrc
 printenv
+set -x
+
+# If the user has mounted the yum repo, we may need to recreate the repo
+pushd /root/yum 
+createrepo .
+popd
 
 #Run spectool on the specfile; grab the source tarball name
-sourcefile=`spectool $1 |awk -F" " '{print $2}'`
-echo $sourcefile
+sourcefile=`spectool $1 |awk -F" " '{print $2}' | head -1`
 
-#Strip the extensions to obtain the directory name
-directory=`basename $sourcefile .tar.gz`
+#Strip the extensions to obtain the directory name: http://stackoverflow.com/questions/14703318/bash-script-remove-extension-from-file-name
+directory=`basename $sourcefile | sed -r 's/\.[[:alnum:]]+\.[[:alnum:]]+$//'`
 echo $directory
-echo $PWD
+workdir=$PWD
+echo $workdir
 
 #copy the sources to the sources dir
 cp -r * /root/rpmbuild/SOURCES 
@@ -22,21 +38,28 @@ cp -r * /root/rpmbuild/SOURCES
 cp $1 /root/rpmbuild/SPECS
 
 #create the source tarball
-cd /root/rpmbuild/SOURCES
-tar zcf $sourcefile --transform "s|^|$directory/|" *
+#cd /root/rpmbuild/SOURCES
+#test tar zcf $directory.tar.gz *
 
 #install the build dependencies according to the specfile
-yum-builddep -y $1
+test yum-builddep -y $1
+
 #build source/binary rpms
-rpmbuild -ba /root/rpmbuild/SPECS/*.spec
+test rpmbuild -ba /root/rpmbuild/SPECS/*.spec
 
 #copy binary rpms to volume
 find /root/rpmbuild/RPMS -type f -exec cp {} /root/yum \;
 
 #createrepo useful? anyway, install the binary RPMs so others may consume this environment
-cd /root/yum
-createrepo .
-yum install -y *.rpm
+#cd /root/yum
+#createrepo .
+#test yum install -y *.rpm
 
 #clean rpmbuild env
 find /root/rpmbuild -type f -exec rm {}  \;
+
+# copy all the rpms into the workdir so they can be grabbed
+cp /root/yum/*.rpm $workdir
+chmod -R a+w /root/yum
+
+set +x
